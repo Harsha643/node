a29057d934a9b6f989da270fffc3fc3e37e1a4ff
Harsha643 <battuharsha6@gmail.com>
2025-02-18 00:21:37 +0530

    file systam

--- practice/FS/file.js
+++ practice/FS/file.js
@@ -1,9 +1,24 @@
-let fs=require("fs")
-fs.writeFileSync("./fs.txt","harsha")
-fs.appendFileSync("./fs.txt","vardhan")
-fs.appendFileSync("./fs.txt","  battu")
-let file=fs.existsSync("./fss.txt")
-fs.renameSync("./fs.js","./file.js")
-let data=fs.readFileSync("./fs.txt","utf-8")
+// let fs=require("fs")
+// fs.writeFileSync("./fs.txt","harsha")
+// fs.appendFileSync("./fs.txt","vardhan")
+// fs.appendFileSync("./fs.txt","  battu")
+// let file=fs.existsSync("./fss.txt")
+// fs.renameSync("./file.js","./fs.js")
+// let data=fs.readFileSync("./fs.txt","utf-8")
+// console.log(data)
+// console.log(file)
+
+let a= require("fs")
+
+a.writeFileSync("./rme.txt","ramsita updaated")
+// a.appendFileSync("./rme.txt","   i know this")
+let ad=a.existsSync("./fs.js")
+a.renameSync("./fs.js","index.js")
+let data=a.readFileSync("./rme.txt","utf-8")
 console.log(data)
-console.log(file)
+console.log(ad)
+
+
+
+
+
