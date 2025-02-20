6291956981a5d844eb253f429713e44052a5b2f2
Harsha643 <battuharsha6@gmail.com>
2025-02-20 16:49:05 +0530

    class

---
+++ practice/parameters/home/api.js
@@ -0,0 +1,29 @@
+
+
+let http=require("http")
+let fs=require("fs")
+let server=http.createServer((req,res)=>{
+let body=""
+    req.on("data",(chunk)=>{
+        body+=chunk
+
+
+    })
+    req.on("end",()=>{
+        console.log(body)
+        fs.writeFile("index.txt",body,(err)=>{
+            if(err){
+                res.write(err.message)
+                res.end()
+            }
+            else{
+                res.write(body)
+                res.end()
+            }
+        })
+        res.end(body)
+    })
+})
+server.listen(3001,()=>{
+    console.log("vasthunna mawa")
+})
--- practice/parameters/quray.js
+++ practice/parameters/quray.js
@@ -4,11 +4,16 @@ let http=require("http")
 
  let server=http.createServer( async(req,res)=>{
 
+
+
+   let ress=url.parse(req.url,true)
+   console.log(ress.query.cat)
+
     let response= await fetch("https://fakestoreapi.com/products")
     let data= await response.json()
-   
- console.log(data)
- 
+   //  console.log(data)
+   res.write(JSON.stringify(data))
+   res.end()
  })
  server.listen(3000,()=>{
     console.log("vasthunna chudu mawa chrome open cheesi ")
